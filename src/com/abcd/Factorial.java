e82457d8ba4254c48388801136553ac7c08a0434
ganeshxyz123 <ganeshthangaraj1997@gmail.com>
2022-09-23 14:32:17 +0530

    c

--- src/com/abcd/Factorial.java
+++ src/com/abcd/Factorial.java
@@ -3,11 +3,11 @@ package com.abcd;
 public class Factorial {
 	public static void main(String[] args) {
 		
-		int b=1;
+		int c=1;
 		
 		for (int i=1; i<=10; i++) {
-			b = b*i;		
+			c = c*i;		
 		}
-		System.out.println(b);
+		System.out.println(c);
 	}		
 }
